f3c39dd3dca4e4b56fb2770ccb8dd7d1eb830edd
Zack Driscoll <zacdrisc@uat.edu>
2020-02-21 12:58:45 -0700

    Added a Player, Tank Controls, and Camera Follow.

---
+++ Stealth Game/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform tf;
+
+    public float rotationSpeed = 1.0f;
+    public float movementSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            tf.position += tf.right * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            tf.position -= tf.right * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            tf.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            tf.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
